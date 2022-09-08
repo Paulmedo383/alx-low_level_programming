044dbf0d238eda229b133edb8321f51626e4b847
paulmedo383 <paulmedo383@gmail.com>
2022-09-08 22:06:59 +0000

    hello

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <studio.h>
+/**
+ *  main - entry point 
+ *
+ *  Return: 0 after printing the function
+ *  */
+int main(viod)
+{puts("/"Programming is like building a multilingual puzzle");
+	return (0);
+}
