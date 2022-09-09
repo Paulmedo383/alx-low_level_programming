8d15ee9b60a50aaefa2f0bbe3470ab323e28af0e
paulmedo383 <paulmedo383@gmail.com>
2022-09-09 10:50:04 +0000

    hello

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 /**
- *  main - entry
- *
- *  Return: 0 after printing the function
+ * main -entry
+ * REturn: 0 after priningthe functions
  */
 int main(void)
 {
-	char Mystr[] = "Programming is like building a multilingual puzzle";
+	char Mystr[] = 'Programming is like building a multilingual puzzle';
 	puts(Mystr);
 	return (0);
 }
