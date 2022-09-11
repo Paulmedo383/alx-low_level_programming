b2479721389e4cb50b73d08f30067cdf50a87d38
paulmedo383 <paulmedo383@gmail.com>
2022-09-11 15:06:48 +0000

    hello

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ *
+ * Return: 0
+ */
+
+int main(void)
+
+{
+	int num;
+	
+	for (num = 0 ; num <= 9 ; num++);
+	{
+		putchar("%d", num);
+	}
+	putchar('\n');
+	return (0);
+}
+
+
+
