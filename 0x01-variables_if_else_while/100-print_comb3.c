e0b314ea1cea798670c76f3498534c6f9ef5cc23
paulmedo383 <paulmedo383@gmail.com>
2022-09-12 06:55:25 +0000

    hello

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - main
+ *
+ * Return: 0
+ */
+
+int mani(void)
+
+{
+	int d1;
+
+	int d2;
+
+	for (d1 = 0 ; d1 < 9 ; d1++)
+	{
+		d2 = d1 + 1;
+	do {
+		putchar(d1 + '0');
+		putchar(d2 + '0');
+		if (d1 < 8)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		d2++;
+	} while (d2 < 10);
+	}
+	putchar('\n');
+	return (0);
+}
