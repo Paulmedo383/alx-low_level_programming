5d19cacda2fe18f1327b5a2770bba979ca07ab4a
paulmedo383 <paulmedo383@gmail.com>
2022-09-16 15:04:41 +0000

    hello

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,22 @@
+#includen "main.c"
+
+/**
+ * main - checks for uppercase character
+ *
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+
+int _isupper(int c);
+{
+	int c;
+
+	if(c >= 'A';)
+	{
+		return(1);
+	}
+	else if(c <='Z';)
+	{
+		reuturn(1);
+	}
+	return(0)
+}
