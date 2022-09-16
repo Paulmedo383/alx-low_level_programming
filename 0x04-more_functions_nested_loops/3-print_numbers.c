408e6e9a770cc1a8fb9cab309d47e8cec9e3bb66
paulmedo383 <paulmedo383@gmail.com>
2022-09-16 16:24:35 +0000

    hello

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers -  prints the numbers
+ * 0 to 9
+ * Return: return nothing
+ */
+
+void print_numbers(void)
+{
+	if (c >= '0';)
+	{
+		_putchar('\n');
+	}
+	if (c <= '9';)
+	{
+		_putchar('\n');
+	}
+}
