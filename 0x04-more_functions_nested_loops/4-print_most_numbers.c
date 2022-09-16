0d2db1010cd05897c801a8b67e6e0ac4be84d229
paulmedo383 <paulmedo383@gmail.com>
2022-09-16 23:18:07 +0000

    hello

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -4,10 +4,20 @@
  * print_most_numbers - prints the numbers
  * 0 to 9
  * except 2 and 4
- * return: 0
  */
 
 void print_most_numbers(void)
 {
+	char i;
+
+	for (i = '0'; i >= '9';)
+	{
+		if (i != '2' && i != '4')
+		{
+			_putchar(i);
+		}
+	}
+	_putchar('\n');
+}
 
 
