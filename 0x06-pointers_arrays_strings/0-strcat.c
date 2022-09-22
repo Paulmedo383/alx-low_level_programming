956254a655cf95e295d769ff71d3c68900466727
paulmedo383 <paulmedo383@gmail.com>
2022-09-22 13:09:04 +0000

    hello

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -3,15 +3,17 @@
 
 /**
  * _strcat - concatenates two strings
+ * @dest: string to append to
  * @src: string to add
- * @dest: string to append
+ *
  * Return: a pointer to the resulting string
  */
-
 char *_strcat(char *dest, char *src)
 {
-	int i;
-	int j;
+	int i, j;
+
+	i = 0;
+	j = 0;
 
 	while (dest[i] != '\0')
 		i++;
